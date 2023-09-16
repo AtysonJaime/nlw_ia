03565ab7a93f688074bdaf3a53837f9f1364346b
Atyson Jaime <atysonjaime@gmail.com>
2023-09-16 01:11:40 -0300

    Update openai.ts

--- upload-ia-api/src/lib/openai.ts
+++ upload-ia-api/src/lib/openai.ts
@@ -2,5 +2,5 @@ import 'dotenv/config'
 import { OpenAI } from 'openai'
 
 export const openai = new OpenAI({
-    apiKey: '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'
+    apiKey: process.env.OPENAI_KEY
 })
